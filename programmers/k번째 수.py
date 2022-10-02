04a578998517f358245dcf213456bc157f2c7369
JINSIL <kjin_048@naver.com>
2022-10-02 15:33:46 +0900

    k번째 수

---
+++ programmers/k번째 수.py
@@ -0,0 +1,16 @@
+
+array=[1, 5, 2, 6, 3, 7, 4]
+commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+
+def solution(array, commands):
+    answer = []
+
+    for i in range(len(commands)):
+        arr = array[commands[i][0] - 1:commands[i][1]]
+        arr.sort()
+        answer.append(arr[commands[i][2] - 1])
+
+    return answer
+
+
+print(solution(array, commands))
