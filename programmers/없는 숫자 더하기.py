8125cd1582268091dcf3b50261e4f69a6259a80f
JINSIL <kjin_048@naver.com>
2022-10-02 16:36:02 +0900

    없는 수 더하기

---
+++ programmers/없는 숫자 더하기.py
@@ -0,0 +1,13 @@
+numbers  = [1,2,3,4,6,7,8,0]
+
+
+def solution(numbers):
+    answer = 0
+
+    for i in range(10):
+        if i not in numbers:
+            answer += i
+    return answer
+
+
+print(solution(numbers))
