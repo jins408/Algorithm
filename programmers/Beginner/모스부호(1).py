9de6072f3753d083dcaab6f6bd8b926144406d16
jins408 <kjin9408@gmail.com>
2023-06-16 15:05:15 +0900

    programmers beginner

---
+++ programmers/Beginner/모스부호(1).py
@@ -0,0 +1,21 @@
+def solution(letter):
+    answer = ''
+
+    letter = letter.split()
+    for i in letter:
+        if i in morse:
+            answer += morse[i]
+
+    return answer
+
+
+morse = {
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+
+letter = ".... . .-.. .-.. ---"
+print(solution(letter))
