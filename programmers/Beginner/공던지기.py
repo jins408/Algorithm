331b8d6f8a2a4ac55bd0b8feb93694e0674d2c18
jins408 <kjin9408@gmail.com>
2023-06-19 21:21:47 +0900

    programmers beginner

--- programmers/Beginner/공던지기.py
+++ programmers/Beginner/공던지기.py
@@ -5,42 +5,17 @@ def solution(numbers, k):
     # 공을 던지는 사람 순서로 변경합니다.((K - 1) * 2)
     # 순환되는 구조이므로 나머지를 통해 위치를 찾습니다. % people
 
-    people = len(numbers)
+    index = 0
+    # k 숫자 하나씩 줄어가면서 index위치 확인
+    while k > 1:
+        index += 2
+        index %= len(numbers)
+        #print(index, end="")
+        k -= 1
+    return numbers[index]
 
-    
-
-
-
-
-
-
-    '''
-    # len(numbers) 짝수일 때
-    if len(numbers)%2 == 0:
-        num = 0
-        while num < k:
-            for i in range(len(numbers)):
-                if i % 2 == 0:
-                    num += 1
-                    answer = numbers[i]
-                    if num == k:
-                        break
-
-    else:
-        num = 0
-        while num < k :
-            answer = numbers[num]
-            num += 2
-            if answer == numbers[-1]:
-                for i in range(1, len(numbers)):
-                    if i%2 != 0:
-                        answer = numbers[i]
-                        if answer == numbers[-1]:
-                            answer = numbers[1]
-    '''
-
-    return answer
-
-numbers=[1, 2, 3,4]
-k=2
+numbers=[1, 2, 3,4,5]
+k=5
 print(solution(numbers, k))
+
+
