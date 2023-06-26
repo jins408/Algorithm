6332264903142015a84ecf642b32c63e9e75ac09
jins408 <kjin9408@gmail.com>
2023-06-26 17:05:37 +0900

    programmers beginner

--- programmers/Beginner/겹치는 선분의 길이.py
+++ programmers/Beginner/겹치는 선분의 길이.py
@@ -17,30 +17,27 @@ def solution(lines):
         result = (sum2 + sum3) - sum1
         return result
 
-    if lines[0][0] < lines[0][1]:
-        if end1 > start2 and start1 < start2:
-            a = end1 - start2
-        else:
-            a = 0
-    if lines[1][0] < lines[1][1]:
-        if end2 > start3 and start2 < start3:
-            b = end2 - start3
-        elif start2 > start3 :
-            b = start2 - start3
-        else:
-            b = 0
-    if lines[2][0] < lines[2][1]:
-        if end3 > start1 and start3 < start1:
-            c = end3 - start1
-        elif end1 > start3:
-            c = end1 - start3
-        else:
-            c = 0
+    if end1 > start2 and start1 < start2:
+        a = end1 - start2
+    else:
+        a = 0
+    if end2 > start3 and start2 < start3:
+        b = end2 - start3
+    elif start2 > start3 :
+        b = start2 - start3
+    else:
+        b = 0
+    if end3 > start1 and start3 < start1:
+        c = end3 - start1
+    elif end1 > start3:
+        c = end1 - start3
+    else:
+        c = 0
 
-        answer = a+b+c
+    answer = a+b+c
 
     return answer
 
-lines = [[0, 5], [3, 9], [1, 10]]
-    #[[0, 5], [3, 9], [1, 10]] [[0, 1], [2, 5], [3, 9]] [[-1, 1], [1, 3], [3, 9]] [[1, 12], [3, 6], [2, 4]]
+lines = [[-1, 1], [1, 3], [3, 9]]
+    #[[0, 5], [3, 9], [1, 10]] [[0, 1], [2, 5], [3, 9]] [[-1, 1], [1, 3], [3, 9]]
 print(solution(lines))
