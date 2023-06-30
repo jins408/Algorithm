bb4e48d96341ba46de9a3e320ef38c4482c18912
jins408 <kjin9408@gmail.com>
2023-06-30 16:55:31 +0900

    programmers beginner

--- programmers/Beginner/다항식 더하기.py
+++ programmers/Beginner/다항식 더하기.py
@@ -1,38 +1,24 @@
 def solution(polynomial):
     answer = ''
 
-    a = polynomial.replace('x','2')
-    b = a.split(' ')
-    print(b)
+    a = polynomial.split(' + ')
+    print(a)
     num = 0
     num2 = 0
-    for i in b:
-        if i[-1] == '2' and len(i) > 1:
-            num += int(i[0:-1])
-            i.isdigit()
-        elif i == '2':
-            num += 1
-        elif i == '1':
-            num2 += 1
-        elif i != '+' and i[-1] != 1:
-            num2 += int(i)
+    for i in a:
+        if i.isdigit():
+            num += int(i)
+        else:
+            if i == 'x':
+                num2 = num2+1
+            else:
+                num2+int(i[:-1])
+            #num2 = num2+1 if i == 'x' else num2 + int(i[:-1])
 
 
-    if num2 != 0 and num != 0:
-        answer = str(num)+'x'+' '+'+'+' '+str(num2)
-    elif num == 0:
-        answer = str(num2)
-    elif num == 2:
-        answer = 'x'
-    elif num2 == '1':
-        answer = str(num2)+' '+str(num)+'x'
-    elif len(str(num)) >= 1:
-        answer = str(num)+'x'
-    else:
-        answer = str(num)+'x'
 
     return answer
 
-polynomial="x + x + x"
+polynomial="3x + 7 + x"
 #"3x + 7 + x"
 print(solution(polynomial))
