71807dc63235a47bb5543f65e0ed104139038ceb
jins408 <kjin9408@gmail.com>
2023-06-30 17:06:39 +0900

    programmers beginner

--- programmers/Beginner/다항식 더하기.py
+++ programmers/Beginner/다항식 더하기.py
@@ -12,13 +12,22 @@ def solution(polynomial):
             if i == 'x':
                 num2 = num2+1
             else:
-                num2+int(i[:-1])
-            #num2 = num2+1 if i == 'x' else num2 + int(i[:-1])
+                num2 = num2+int(i[:-1])
+
+    if num2 == 0:
+        return str(num)
+    elif num2 == 1:
+        if num != 0:
+            return 'x + '+str(num)
+        else:
+            return 'x'
+    else:
+        if num != 0:
+            return f'{num2}x + {num}'
+        else:
+            return f'{num2}x'
 
 
-
-    return answer
-
-polynomial="3x + 7 + x"
+polynomial="x + x + x"
 #"3x + 7 + x"
 print(solution(polynomial))
