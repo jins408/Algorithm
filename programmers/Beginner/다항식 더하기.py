a04fa1d7e4b2d575a80eededbcf974071282ddbc
jins408 <kjin9408@gmail.com>
2023-07-02 23:07:02 +0900

    programmers beginner

--- programmers/Beginner/다항식 더하기.py
+++ programmers/Beginner/다항식 더하기.py
@@ -16,15 +16,20 @@ def solution(polynomial):
             else:
                 # 3x, 12x 이런식이면 앞에 숫자 값만 가져오도록 [:-1]슬라이싱 해주고 num2에 더해줌
                 num2 = num2+int(i[:-1])
-    
+
+    # num2가 0면이 x계수가 없는 것이므로 num만 return
     if num2 == 0:
         return str(num)
+    # num2가 1이면 x만 return해주면 됨 x는 1값을 의미
     elif num2 == 1:
+        # num이 0이 아니면 x+num 으로 return 해줘야 하므로
         if num != 0:
             return 'x + '+str(num)
+        # num 0 이면 x만 return
         else:
             return 'x'
     else:
+        # num2가 0,1이 아닐때, 그리고 num이 값이 있을때 나눠서 return해줌
         if num != 0:
             return f'{num2}x + {num}'
         else:
