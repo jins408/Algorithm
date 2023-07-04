8a5d2cf6ac0ce0a86ed303d9cb09e0462fc8cb94
jins408 <kjin9408@gmail.com>
2023-07-04 15:34:07 +0900

    programmers beginner

---
+++ programmers/Level1/문자열p와 y의 개수.py
@@ -0,0 +1,20 @@
+def solution(s):
+
+    p = 0
+    y = 0
+
+    for i in s:
+        if i == 'p' or i == 'P':
+            p += 1
+        elif i == 'y' or i == 'Y':
+            y += 1
+
+    if y == p:
+        answer = True
+    else:
+        answer = False
+
+    return answer
+
+s = "pPoooyY"
+print(solution(s))
