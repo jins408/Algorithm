1b1b3725739f7d68febc020d6241a484aa962566
jins408 <kjin9408@gmail.com>
2023-07-05 23:24:39 +0900

    programmers Level1

---
+++ programmers/Level1/같은 숫자는 싫어.py
@@ -0,0 +1,14 @@
+def solution(arr):
+
+    answer = [arr[0]]
+
+    for i in range(1, len(arr)):
+        if arr[i] != arr[i-1]:
+            answer.append(arr[i])
+
+
+    return answer
+
+arr =[4,4,4,3,3]
+    #[1,1,3,3,0,1,1]
+print(solution(arr))
