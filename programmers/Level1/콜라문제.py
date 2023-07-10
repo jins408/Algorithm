4ca0dfe9fcae07b0c470ac251eea18d0edbd3306
jins408 <kjin9408@gmail.com>
2023-07-10 18:59:48 +0900

    programmers Level1

---
+++ programmers/Level1/콜라문제.py
@@ -0,0 +1,32 @@
+def solution(a, b, n):
+    answer = 0
+
+    coke = n
+    while coke <= n:
+        # 먼저 몫과 나머지를 구해 줌
+        mod = coke % a
+        coke //= a
+        # a의 개수만큼 마트에서 돌려주는 콜라병 수 체크
+        coke *= b
+        # 먼저 몫값을 answer에 더해줌
+        answer += coke
+        # 해당 조건에 걸리면 더 이상 a개수만큼 나눌 수 있는 빈병이 존재하지 않음(몫과 나머지 모두 체크하는 조건)
+        # 몫이 b보다 작으면 더 이상 돌려받을 수 없는 콜라가 없다는 것
+        # 몫이 a로 나눈 나머지가 0이면 더 이상 돌려 받을 수 있는 콜라가 없다는 것
+        if coke <= b and coke%a == 0:
+            break
+        else:
+            # 나머지와 몫값을 더해서 또 a개수만큼 나눠주기 위해 더해 줌
+            coke += mod
+
+    return answer
+
+
+# 콜라를 받기 위해 마트에 주어야 하는 병 수
+a = 3
+# 빈 병 a개를 가져다 주면 마트가 주는 콜라 병 수
+b = 1
+# 상빈이가 가지고 있는 빈 병의 개수
+n = 20
+
+print(solution(a, b, n))
--- programmers/Level1/푸드파이트대화.py
+++ programmers/Level1/푸드파이트대화.py
@@ -2,10 +2,13 @@ def solution(food):
     answer = ''
 
     for i in range(1, len(food)):
+        # 짝수,홀수 인지 먼저 구분 ex)food[i]=3이면 두 선수가 하나씩 나눠 가질 수 있음(나머지 1은 2명이 나눌 수 없는 음식)
         a = food[i]//2
+        # a의 수만큼 반복해서 빈문자열에 숫자를 넣어줌
         for j in range(a):
             answer += str(i)
     print(answer)
+    # 두 선수가 똑같이 나눠 가지기 때문에, 0(물)을 기준으로 거꾸로 숫를 돌려서 문자열에 붙여줌
     a = answer[::-1]
     answer = answer+"0"+a
 
