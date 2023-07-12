d431eae62922014d2f4d27be77cb27e2140664c5
jins408 <kjin9408@gmail.com>
2023-07-13 00:20:01 +0900

    programmers Level1

--- programmers/Level1/소수 만들기.py
+++ programmers/Level1/소수 만들기.py
@@ -7,8 +7,9 @@ def fun(num):
         for j in range(2,max_num+1):
             if num[i]%j == 0:
                 sum += 1
-            if sum > 2:
+            if sum >= 2:
                 break
+            # 쟈기 자신과 똑같은 수를 가진것만 소수이므로 j(인덱스)와 num[i] 같을때 cnt+1
             if sum == 1 and j == num[i]:
                 cnt +=1
     return cnt
@@ -19,9 +20,11 @@ def solution(nums):
     # 3개씩 더한 수
     num = []
     for i in range(0, len(nums)-2):
-        for j in range(i+2,len(nums)):
-            num.append(nums[i]+nums[i+1]+nums[j])
+        for j in range(i+1,len(nums)):
+            for k in range(j+1, len(nums)):
+                num.append(nums[i]+nums[j]+nums[k])
     print(num)
+    # 소수를 찾는 함수
     get_fun = fun(num)
 
     return get_fun
