1bd43c13f1c8da7ef0e99d243431428ee2067a2d
jins408 <kjin9408@gmail.com>
2023-08-15 01:02:17 +0900

    programmers Level1

--- programmers/Level1/공원 산책.py
+++ programmers/Level1/공원 산책.py
@@ -1,16 +1,9 @@
 def solution(park, routes):
-    answer = []
-
-    dic_routes={}
-    for route in routes:
-        k, v = route.split()
-        dic_routes[k] = int(v)
-    print(dic_routes)
-
     # 위치
     r = 0
     c = 0
 
+    # 'S'가 있는 곳이 좌표의 시작위치
     for i in range(len(park)):
         for j in range(len(park[i])):
             if park[i][j] == 'S':
@@ -18,38 +11,41 @@ def solution(park, routes):
                 c = j
                 break
     # 이동
-    for route in dic_routes:
+    for route in routes:
+        # if,elif에 걸리지 않으면 좌표 초기화
         nr = r
         nc = c
-        for k in range(dic_routes[route]):
-            # 동
-            if route == 'E' and nc < len(park[0])-1 and park[nr][nc+1] != 'X':
+        for k in range(int(route[2])): # route="E 2"일때,route[0]='E' route[2]=2
+            # route[2]만큼 for문을 돌면서 좌표를 찾아준다.
+            # 동 (동쪽으로 이동했을때, 동쪽 맨끝쪽이면 안되고, 장애물을 만나서도 안됨)
+            if route[0] == 'E' and nc < len(park[0])-1 and park[nr][nc+1] != 'X':
                 nc += 1
-                if k == dic_routes[route]-1:
+                if k == int(route[2])-1:
+                    # k가 route[2]와 같아지면 해당하는 if문 종료
                     c = nc
-            # 서
-            elif route == 'W' and nc > 0 and park[nr][nc-1] != 'X':
+            # 서 (서쪽으로 이동했을때, 서쪽 맨끝쪽이면 안되고, 장애물을 만나서도 안됨)
+            elif route[0] == 'W' and nc > 0 and park[nr][nc-1] != 'X':
                 nc-=1
-                if k == dic_routes[route]-1:
+                if k == int(route[2])-1:
                     c = nc
-            # 남
-            elif route == 'S' and nr < len(park)-1 and park[nr+1][nc] != 'X':
+            # 남 (남쪽으로 이동했을때, 남쪽 맨끝쪽이면 안되고, 장애물을 만나서도 안됨)
+            elif route[0] == 'S' and nr < len(park)-1 and park[nr+1][nc] != 'X':
                 nr += 1
-                if k == dic_routes[route]-1:
+                if k == int(route[2])-1:
                     r = nr
-            # 북
-            elif route == 'N' and nr > 0 and park[nr-1][nc] != 'X':
+            # 북 (북쪽으로 이동했을때, 북쪽 맨끝쪽이면 안되고, 장애물을 만나서도 안됨)
+            elif route[0] == 'N' and nr > 0 and park[nr-1][nc] != 'X':
                 nr -= 1
-                if k == dic_routes[route]-1:
+                if k == int(route[2])-1:
                     r = nr
 
     return [r, c]
 
-park = ["OXXO", "XSXO", "XXXX"]
+park = ["SOO","OOO","OOO"]
     #["OSO","OOO","OXO","OOO"]
     #["SOO","OXX","OOO"]
     #["SOO","OOO","OOO"]
-routes =["E 1", "S 1"]
+routes =["E 2","S 2","W 1"]
     #["E 2","S 3","W 1"]
     #["E 2","S 2","W 1"]
     #["E 2","S 2","W 1"]
--- programmers/Level1/공원산책1.py
+++ programmers/Level1/공원산책1.py
@@ -1,51 +1,39 @@
 def solution(park, routes):
-    # 위치 index
-    x = 0
-    y = 0
-
-    # 시작 위치 찾기
+    r,c = 0,0
     for i in range(len(park)):
         for j in range(len(park[i])):
             if park[i][j] == 'S':
-                x = j
-                y = i
+                r,c = i,j
+
+    # 북,남,서,동
+    dic = {'N':(-1, 0), 'S':(1, 0), 'W':(0, -1), 'E':(0, 1)}
+
+    for i in routes:
+        #print(i.split()[0])
+        dr, dc = dic[i.split()[0]] # 이동방향
+        n = int(i.split()[1])   # 이동횟수
+
+        # 현재위치
+        s_r = r
+        s_c = c
+        for _ in range(n):
+            nr = r+dr #현재위치에서 이동방향만큼 더해준다
+            nc = c+dc
+            if 0<=nr<=len(park)-1 and 0<=nc<=len(park[0])-1 and park[nr][nc] != 'X':
+                # 조건에 맞으면 이동방향이 현재 위치가 되고
+                r,c = nr,nc
+            else:
+                # 조건에 맞지않으면 현재위치로 다시 초기화 시켜준다
+                r,c = s_r,s_c
                 break
 
-    # 이동
-    for route in routes:
-        # 위치 초기화
-        xx = x
-        yy = y
-        # 이동 - 장애물이 있거나 공원을 벗어나면 명령 무시
-        for step in range(int(route[2])):
-            # 동쪽 : 현재 위치가 map 가장 오른쪽이면 안됨, 이동할 곳이 장애물이면 안됨
-            if route[0] == 'E' and xx != len(park[0]) - 1 and park[yy][xx + 1] != 'X':
-                xx += 1
-                if step == int(route[2]) - 1:
-                    x = xx  # step만큼 움직였으면 위치 초기화
-            # 서쪽 : 현재 위치가 map 가장 왼쪽이면 안됨, 이동할 곳이 장애물이면 안됨
-            elif route[0] == 'W' and xx != 0 and park[yy][xx - 1] != 'X':
-                xx -= 1
-                if step == int(route[2]) - 1:
-                    x = xx
-            # 남쪽 : 현재 위치가 map 가장 아래쪽이면 안됨, 이동할 곳이 장애물이면 안됨
-            elif route[0] == 'S' and yy != len(park) - 1 and park[yy + 1][xx] != 'X':
-                yy += 1
-                if step == int(route[2]) - 1:
-                    y = yy
-            # 북쪽 : 현재 위치가 map 가장 위쪽이면 안됨, 이동할 곳이 장애물이면 안됨
-            elif route[0] == 'N' and yy != 0 and park[yy - 1][xx] != 'X':
-                yy -= 1
-                if step == int(route[2]) - 1:
-                    y = yy
+    return [r, c]
 
-    return [y, x]
-
-park =  ["OSO","OOO","OXO","OOO"]
+park = ["OSO","OOO","OXO","OOO"]
     #["OSO","OOO","OXO","OOO"]
     #["SOO","OXX","OOO"]
     #["SOO","OOO","OOO"]
-routes =  ["E 2","S 3","W 1"]
+routes =["E 2","S 3","W 1"]
     #["E 2","S 3","W 1"]
     #["E 2","S 2","W 1"]
     #["E 2","S 2","W 1"]
