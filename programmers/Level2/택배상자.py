5a26c6bf1e9151f4d56d518b3424fbee6fd6fce0
jins408 <kjin9408@gmail.com>
2023-09-10 23:59:13 +0900

    programmers Level2

--- programmers/Level2/택배상자.py
+++ programmers/Level2/택배상자.py
@@ -2,12 +2,13 @@ def solution(order):
     answer = 0
 
     stack = []
-    for idx, num in enumerate(order):
-        stack.append(idx+1)
 
-        while stack and stack[-1] == order[answer]:
-            stack.pop()
-            answer +=1
+    for idx, num in enumerate(order):
+        stack.append(idx+1) # stack에 인덱스 값을 넣는다(인덱스는 1부터) -> 택배상자가 1번부터 순서대로 들어가기 때문에
+
+        while stack and stack[-1] == order[answer]: # stack에 뽑은수와 order의 수가 일치하면
+            stack.pop() # pop해주고
+            answer +=1  # +1을 해준다
 
     return answer
 
