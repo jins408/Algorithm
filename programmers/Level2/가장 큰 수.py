5cd8b7be9b37ae40d2f3b226907e94bc77a26c04
jins408 <kjin9408@gmail.com>
2023-09-11 20:57:14 +0900

    programmers Level2

---
+++ programmers/Level2/가장 큰 수.py
@@ -0,0 +1,16 @@
+def solution(numbers):
+    answer = ''
+
+    numbers = [str(i) for i in numbers]
+
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    print(numbers)
+
+    for i in numbers:
+        answer += i
+
+    return str(int(answer))
+
+numbers = [3, 30, 34, 5, 9]
+    #[6, 10, 2]
+print(solution(numbers))
