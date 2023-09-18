27a8d12a98fcbe35a935496c9fb1a1b69dccf5df
jins408 <kjin9408@gmail.com>
2023-09-18 23:19:47 +0900

    Review

--- Review/큰 수 만들기.py
+++ Review/큰 수 만들기.py
@@ -5,15 +5,16 @@ def solution(number, k):
 
     for num in number:
 
-        while stack and stack[-1] < num:
+        while k>0 and stack and stack[-1] < num:
             stack.pop()
             k -= 1
         stack.append(num)
 
-    answer = ''.join(stack)
+    answer = ''.join(stack[:len(stack)-k])
+    # print(''.join(stack)) -> 마지막 테스트케이스 실패
 
     return answer
 
-number = "1924"
-k = 2
+number = "1231234"
+k = 3
 print(solution(number, k))
