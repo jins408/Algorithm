696d55a1022d497231e4cf2917235d1c80407da2
jins408 <kjin9408@gmail.com>
2023-09-18 23:14:16 +0900

    programmers Level2

--- programmers/Level2/큰 수 만들기(stack).py
+++ programmers/Level2/큰 수 만들기(stack).py
@@ -4,12 +4,12 @@ def solution(number, k):
     stack = []
 
     for n in number:
-        while k > 0 and stack and stack[-1] < n:
+        while stack and stack[-1] < n:
             stack.pop()
             k -= 1
         stack.append(n)
 
-    answer = ''.join(stack[:len(stack)-k])
+    answer = ''.join(stack)
 
     return answer
 
