fcb7d32cfe0e5bc011f3719b2974267024f476a2
jins408 <kjin9408@gmail.com>
2023-10-23 23:01:49 +0900

    Programmers Level2

---
+++ programmers/Level2/행렬 테두리 회전하기.py
@@ -0,0 +1,37 @@
+
+def solution(rows, columns, queries):
+    answer = []
+
+    arr = [[0]*columns for _ in range(rows)]
+    num = 1
+    for i in range(rows):
+        for j in range(columns):
+            arr[i][j] = num
+            num += 1
+   # print(arr)
+
+    for x1,y1,x2,y2 in queries:
+        temp = arr[x1-1][y1-1]
+        mini = temp
+
+        # 왼쪽 세로
+        for k in range(x1 - 1, x2 - 1):
+            test = arr[k + 1][y1 - 1]
+            arr[k][y1 - 1] = test
+            mini = min(mini, test)
+
+        # 하단 가로
+
+        # 왼쪽 세로
+
+
+        # 상단 가로
+
+
+
+    return answer
+
+rows = 6
+columns = 6
+queries = [[2,2,5,4],[3,3,6,6],[5,1,6,3]]
+print(solution(rows, columns, queries))
