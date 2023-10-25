09e4ce97e8a780ab359e20bcbf36e8cd5da69dd5
jins408 <kjin9408@gmail.com>
2023-10-25 18:41:49 +0900

    Programmers Level2

--- programmers/Level2/줄서는 방법.py
+++ programmers/Level2/줄서는 방법.py
@@ -8,10 +8,14 @@ def my_factorial(n):
 def solution(n, k):
     answer = []
 
+    # answer에 담기는 맨 처음 수는 k//(n - 1)+1
+    # 이게 정해 졌으면 그 뒤에 순서대로 줄 세우는 경우의 수가 (n - 1)!
     num_list = [i for i in range(1, n+1)]
     while (n != 0):
         s = my_factorial(n-1)
+        # k에서 (n - 1)!을 나눴을 때의 몫이 첫번째 오는 자리
         idx = k//s
+        # k에서 (n - 1)!을 나눴을 때의 나머지가 다시 k로 오는 자리
         k = k%s
         if k == 0:
             answer.append(num_list.pop(idx-1))
