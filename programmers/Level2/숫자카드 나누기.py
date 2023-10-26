517d54696da61c6069f76704e2eee7bfb36e8856
jins408 <kjin9408@gmail.com>
2023-10-26 21:47:02 +0900

    Programmers Level2

--- programmers/Level2/숫자카드 나누기.py
+++ programmers/Level2/숫자카드 나누기.py
@@ -1,14 +1,17 @@
+# 최대공약수
 def gcd_(a, b):
     while b > 0:
         a,b = b, a%b
     return a
 
+# N개의 최대공약수 구하기
 def gcd_N(arr):
     gcd = arr[0]
     for i in arr:
         gcd = gcd_(gcd, i)
     return gcd
 
+# 조건에 따라서 나눠어 떨어지는이 아닌지 확인
 def div(num,array):
     for i in array:
         if i%num == 0:
@@ -21,8 +24,9 @@ def solution(arrayA, arrayB):
     num_a = gcd_N(arrayA)
     num_b = gcd_N(arrayB)
 
-    # 첫 번째 조건
+    # 첫 번째 조건 div가 True일때만 확인
     if div(num_a, arrayB):
+        # 둘중에 큰 값을 찾아 줘야 하므로 max로 큰값을 answer에 넣어주면서 큰 양수 값을 찾음
         answer = max(answer, num_a)
 
     # 두 번째 조건
