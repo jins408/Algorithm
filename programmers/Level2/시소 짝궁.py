e035ccce8e27a5e5f61d0586badd5d1c5c69e6b1
jins408 <kjin9408@gmail.com>
2023-10-30 20:12:37 +0900

    Programmers Level2

--- programmers/Level2/시소 짝궁.py
+++ programmers/Level2/시소 짝궁.py
@@ -12,12 +12,15 @@ def solution(weights):
     # 해당하는 키에 일치하는 값이 있는지 확인
     for i in dic:
         if dic[i] > 1:
+            # 정수롤 +1 해주기위해 /2를 해줌
             answer += (dic[i]*(dic[i]-1)) / 2
         if i*2 in dic:
             answer += dic[i]*dic[2*i]
         if i*2/3 in dic:
+            # 정수롤 +1 해주기위해 /3를 해줌
             answer += dic[i]*dic[i*2/3]
         if i*3/4 in dic:
+            # 정수롤 +1 해주기위해 /4를 해줌
             answer += dic[i]*dic[i*3/4]
 
 
